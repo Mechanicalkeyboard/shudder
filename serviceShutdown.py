bc78848f7253348c7d7d5f139152783adab1b502
Jonathan Daigle <jonathan.armel.daigle@gmail.com>
2018-07-17 16:58:19 -0300

    adding local host

--- serviceShutdown.py
+++ serviceShutdown.py
@@ -3,12 +3,12 @@ import time
 
 try:
     print("trying localhost")
-    url = 'http://127.0.0.1:8080/shutdown'
+    url = 'http://localhost:8080/shutdown'
     response = requests.post(url)
     print(response.status_code)
     statusCode = 200
     while statusCode == 200 or statusCode == 503:
-        dockerPs = 'http://127.0.0.1:8080/status'
+        dockerPs = 'http://localhost:8080/status'
         status = requests.get(dockerPs)
         statusCode = status.status_code
         print(statusCode)
