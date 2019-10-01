31a8b20b26be903a81173a3685da25da10760060
PULL REQUEST TEST <noreply@cxengagelabs.net>
2019-10-01 17:58:31 -0300

    adding change long

---
+++ CHANGELOG.md
@@ -0,0 +1,19 @@
+# Change Log
+All notable changes to this project will be documented in this file. This change log follows the conventions of [keepachangelog.com](http://keepachangelog.com/).
+
+## [Unreleased]
+
+- Adding greenbay_version tags to infra/service deploy
+- Add subdomain for variables to be used in consul-kv, main.tf., and add sbudomain declaration.
+
+## [0.3.0] - 2019-10-01
+### Changed
+- Shutdown Eureka
+
+## [0.2.0] - 2019-10-01
+### Changed
+- Changelog Start
+
+
+[Unreleased]: https://github.com/SerenovaLLC/sudder/compare/0.3.0...0.4.0-SNAPSHOT
+[0.3.0]: https://github.com/SerenovaLLC/terraform-modules/tag/0.3.0
--- serviceShutdown.py
+++ serviceShutdown.py
@@ -1,7 +1,26 @@
 import requests
+import os
 import time
-
 try:
+    print("Getting AMI ID")
+    ami_id_route='http://169.254.169.254/latest/meta-data/ami-id'
+    ami_id = requests.get(ami_id_route)
+    ami_id='i-0dd87e2cff5cf660c'
+    service=os.environ['service']
+    print(service)
+    region=os.environ['region']
+    print(region)
+    environment=os.environ['environment']
+    print(environment)
+    if environment == 'prod': 
+        domain = "cxengage.net"
+    else:
+        domain = "cxengagelabs.net"
+    eureka_url='http://' + region + '-' + environment + '-eureka.'+ domain +':8080/eureka/v2/apps/' + service + '/' + ami_id + '/status?value=DOWN'
+    print(eureka_url)
+    eureka=requests.put(eureka_url)
+    print(eureka)
+    time.sleep(30)
     print("trying localhost")
     url = 'http://localhost:8080/shutdown'
     response = requests.post(url)
@@ -11,7 +30,7 @@ try:
         dockerPs = 'http://localhost:8080/status'
         status = requests.get(dockerPs)
         statusCode = status.status_code
-        print(statusCode)
+        print(statusCode)   
         time.sleep(5)
     print(status.status_code)
 except: 
